2c9a4373baa533e41201007899d3bdc8f1371369
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-05-26 23:33:49 +0200

    login/signup for customers

---
+++ frontend/src/customer/pages/Auth/Auth.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import LoginForm from './LoginForm'
+import { Alert, Button, Snackbar } from '@mui/material';
+import SignupForm from './SignupForm';
+import { useAppSelector } from '../../../State/Store';
+
+const Auth = () => {
+    const [isLoginPage, setIsLoginPage] = useState(true);
+    const handleCloseSnackbar = () => setSnackbarOpen(false);
+    const { auth } = useAppSelector(store => store);
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+    useEffect(() => {
+
+        if (auth.otpSent || auth.error) {
+            setSnackbarOpen(true);
+            console.log("store ", auth.error)
+        }
+
+    }, [auth.otpSent,auth.error])
+
+    return (
+        <div className='flex justify-center h-[90vh] items-center'>
+            <div className='max-w-md h-[85vh] rounded-md border shadow-lg '>
+                <img className='w-full rounded-t-md' src="https://i.ibb.co/xK505dxR/banner.png" alt="Banner" />
+                <div className='mt-8 px-10'>
+                    {isLoginPage ? <LoginForm /> : <SignupForm />}
+
+                    <div className='flex items-center justify-center mt-5'>
+                        <p>{isLoginPage && "Don't"} have Account ?
+                            <Button onClick={() => setIsLoginPage(!isLoginPage)} sx={{color: 'black', fontSize: '11px' }}>{isLoginPage ? "Create account" : "Login"}</Button>
+                        </p>
+                        
+                    </div>
+                </div>
+            </div>
+            <Snackbar
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                open={snackbarOpen} autoHideDuration={6000}
+                onClose={handleCloseSnackbar}
+            >
+                <Alert
+                    onClose={handleCloseSnackbar}
+                    severity={auth.error?"error":"success"}
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                >
+                    {auth.error?auth.error : " otp sent to your email!"}
+                </Alert>
+            </Snackbar>
+        </div>
+    )
+}
+
+export default Auth
---
+++ frontend/src/customer/pages/Auth/LoginForm.tsx
@@ -0,0 +1,147 @@
+import { Button, CircularProgress, TextField } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../State/Store';
+import { useFormik } from 'formik';
+import { sendLoginSignupOtp, signin } from '../../../State/AuthSlice';
+import OTPInput from '../../components/OtpField/OTPInput';
+
+const LoginForm = () => {
+
+    const navigate = useNavigate();
+    const [otp, setOtp] = useState("");
+    const [isOtpSent, setIsOtpSent] = useState(false)
+    const [timer, setTimer] = useState<number>(30); // Timer state
+    const [isTimerActive, setIsTimerActive] = useState<boolean>(false);
+    const dispatch = useAppDispatch();
+    const { auth } = useAppSelector(store => store)
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            otp: ''
+        },
+
+        onSubmit: (values: any) => {
+            // Handle form submission
+            dispatch(signin({ email: values.email, otp, navigate }))
+            console.log('Form data:', values);
+        }
+    });
+
+    const handleOtpChange = (otp: any) => {
+
+        setOtp(otp);
+
+    };
+
+    const handleResendOTP = () => {
+        // Implement OTP resend logic
+        dispatch(sendLoginSignupOtp({ email: "signing_"+formik.values.email }))
+        console.log('Resend OTP');
+        setTimer(30);
+        setIsTimerActive(true);
+    };
+
+    const handleSentOtp = () => {
+        setIsOtpSent(true);
+        handleResendOTP();
+    }
+
+    const handleLogin = () => {
+        formik.handleSubmit()
+    }
+
+  
+
+    useEffect(() => {
+        let interval: NodeJS.Timeout | undefined;
+
+        if (isTimerActive) {
+            interval = setInterval(() => {
+                setTimer(prev => {
+                    if (prev === 1) {
+                        clearInterval(interval);
+                        setIsTimerActive(false);
+                        return 30; // Reset timer for next OTP request
+                    }
+                    return prev - 1;
+                });
+            }, 1000);
+        }
+
+        return () => {
+            if (interval) clearInterval(interval);
+        };
+    }, [isTimerActive]);
+
+
+
+    return (
+        <div>
+            <h1 className='text-center font-bold text-xl text-primary-color pb-5'>Login</h1>
+            <form className="space-y-5">
+
+                <TextField
+                    fullWidth
+                    name="email"
+                    label="Enter Your Email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email ? formik.errors.email as string : undefined}
+                />
+
+                {auth.otpSent && <div className="space-y-2">
+                    <p className="font-medium text-sm">
+                        * Enter OTP sent to your mobile number
+                    </p>
+                    <OTPInput
+                        length={6}
+                        onChange={handleOtpChange}
+                        error={false}
+                    />
+                    <p className="text-xs space-x-2">
+                        {isTimerActive ? (
+                            <span>Resend OTP in {timer} seconds</span>
+                        ) : (
+                            <>
+                                Didn't receive OTP?{" "}
+                                <span
+                                    onClick={handleResendOTP}
+                                    className="text-gray-700 cursor-pointer hover:text-black font-semibold"
+                                >
+                                    Resend OTP
+                                </span>
+                            </>
+                        )}
+                    </p>
+                    {formik.touched.otp && formik.errors.otp && <p>{formik.errors.otp as string}</p>}
+                </div>}
+
+                {auth.otpSent && <div>
+                    <Button disabled={auth.loading} onClick={handleLogin}
+                        fullWidth variant='contained' sx={{ py: "11px" }}>{
+                            auth.loading ? <CircularProgress  />: "Login"}</Button>
+                </div>}
+
+                {!auth.otpSent && <Button
+                disabled={auth.loading}
+                    fullWidth
+                    variant='contained'
+                    onClick={handleSentOtp}
+                    sx={{ py: "11px", backgroundColor: "black", color: "white" }}>{
+                        auth.loading ? <CircularProgress  />: "send otp"}</Button>
+                }
+
+
+
+            </form>
+
+         
+        </div>
+    )
+}
+
+export default LoginForm
