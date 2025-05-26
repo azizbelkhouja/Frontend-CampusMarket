77f9ab867170fbcce20217f3d4bfc7bcf516283b
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-05-26 23:30:37 +0200

    fetching data from backend to frontend
    redux toolkit, axios, slices, types

--- frontend/src/State/Store.ts
+++ frontend/src/State/Store.ts
@@ -1,9 +1,50 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { thunk } from "redux-thunk";
-
+import AuthSlice from "./AuthSlice";
+import UserSlice from "./customer/UserSlice";
+import ProductSlice from "./customer/ProductSlice";
+import CartSlice from "./customer/CartSlice";
+import OrderSlice from "./customer/OrderSlice";
+import CouponSlice from "./customer/CouponSlice";
+import ReviewSlice from "./customer/ReviewSlice";
+import WishlistSlice from "./customer/WishlistSlice";
+import CustomerSlice from "./customer/Customer/CustomerSlice";
+import sellerAuthenticationSlice from "./seller/sellerAuthenticationSlice";
+import sellerProductSlice from "./seller/sellerProductSlice";
+import sellerOrderSlice from "./seller/sellerOrderSlice";
+import payoutSlice from "./seller/payoutSlice";
+import transactionSlice from "./seller/transactionSlice";
+import revenueChartSlice from "./seller/revenueChartSlice";
+import sellerSlice from './seller/sellerSlice';
 
 const rootReducer = combineReducers({
+  
+// customer
+auth: AuthSlice,
+user: UserSlice,
+product: ProductSlice,
+cart: CartSlice,
+orders: OrderSlice,
+coupone: CouponSlice,
+review: ReviewSlice,
+wishlist: WishlistSlice,
+homePage:CustomerSlice,
+
+// seller
+sellers: sellerSlice,
+sellerAuth: sellerAuthenticationSlice,
+sellerProduct: sellerProductSlice,
+sellerOrder: sellerOrderSlice,
+payouts: payoutSlice,
+transaction: transactionSlice,
+revenueChart: revenueChartSlice,
+
+// admin
+adminCoupon:AdminCouponSlice,
+adminDeals:DealSlice,
+admin:AdminSlice,
+deal:DealSlice
 
 });
 
@@ -18,3 +59,4 @@ export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
+  
