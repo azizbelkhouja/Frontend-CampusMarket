96b992d94b95c42847b9cf7f04b49a161268db7d
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-05-31 00:27:15 +0200

    ShopByCategory

--- frontend/src/customer/pages/Home/ShopByCategory/ShopByCategoryCard.tsx
+++ frontend/src/customer/pages/Home/ShopByCategory/ShopByCategoryCard.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import "./ShopByCategory.css"
+import { useNavigate } from 'react-router-dom';
 
-const ShopByCategoryCard = () => {
+const ShopByCategoryCard = ({item}:any) => {
+
+  const navigate = useNavigate();
 
   return (
-    <div className='custom-border flex gap-3 flex-col justify-center items-center group cursor-pointer'>
+    <div onClick={()=>navigate(`/products/${item.categoryId}`)} className='custom-border flex gap-3 flex-col justify-center items-center group cursor-pointer'>
       <div className='w-[120px] h-[250px] rounded-full lg:w-[220px] lg:h[400px]'>
-        <img className='object-cover object-top h-full w-full' src="https://m.media-amazon.com/images/I/71F5H-S4OyL._AC_UF1000,1000_QL80_.jpg" alt="Book" />
+        <img className='object-cover object-top h-full w-full' src={item.image} alt={item.name} />
       </div>
-      <h1>Typescript Books</h1>
+      <h1>{item.name}</h1>
     </div>
   )
 }
