24e1aca4ebc77f01c53b1cf5c08edd618200c645
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-05-31 00:22:30 +0200

    deal data fetching

--- frontend/src/customer/pages/Home/Deal/Deal.tsx
+++ frontend/src/customer/pages/Home/Deal/Deal.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import DealCard from './DealCard';
+import { useAppSelector } from '../../../../State/Store';
 
 const DealSlider = () => {
+
+  const {homePage} = useAppSelector(store => store);
 
   return (
     <div className="py-5 lg:px-20">
       <div className="flex items-center justify-between gap-2">
-        {[1,1,1,1,1,1].map(() => ( <DealCard /> ))}
+        {homePage.homePageData?.deals?.map((item:Deal) => 
+          <div className="border flex flex-col items-center justify-center">
+            <DealCard deal = {item}/>
+          </div>)}
       </div>
     </div>
   )
--- frontend/src/customer/pages/Home/Deal/DealCard.tsx
+++ frontend/src/customer/pages/Home/Deal/DealCard.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import type { Deal } from '../../../../types/dealTypes';
 
-const DealCard = () => {
+const DealCard = ({deal}:{deal:Deal}) => {
+
+  const navigate = useNavigate();
 
   return (
-    <div className='w-[12rem] cursor-pointer'>
-        <img className='border-x-[7px] border-t-[7px] w-full h-[12rem] object-cover object-top border-black  p-2' src="https://cdn.sandberg.world/products/images/lg/640-97_lg.jpg" alt="office chair" />
+    <div onClick={()=>navigate(`/products/${deal.category.categoryId}`)} className='w-[12rem] cursor-pointer'>
+        <img className='border-x-[7px] border-t-[7px] w-full h-[12rem] object-cover object-top border-black  p-2' src={deal.category.image} alt="office chair" />
         <div className='border-4 border-black bg-black text-white p-2 text-center'>
-          <p className='text-lg font-semibold'>Best Chair in Italy</p>
-          <p className='text-2xl font-bold'>29% OFF</p>
+          <p className='text-lg font-semibold'>{deal.category.categoryId.split("_").join(" ")}</p>
+          <p className='text-2xl font-bold'>{deal.discount}% OFF</p>
           <p className='text-balance text-lg bg-white text-black mt-2 font-medium'>Shop Now</p>
         </div>
     </div>
