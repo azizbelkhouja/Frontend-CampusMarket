702449fda84e810485f61a6fe3d664dfc799af1f
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-05-31 00:24:59 +0200

    ElectronicsCategory data

--- frontend/src/customer/pages/Home/ElectronicsCategory/ElectronicsCategory.tsx
+++ frontend/src/customer/pages/Home/ElectronicsCategory/ElectronicsCategory.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ElectronicsCategoryCard from './ElectronicsCategoryCard'
+import { useAppSelector } from '../../../../State/Store';
 
 const ElectronicsCategory = () => {
+
+  const {homePage}=useAppSelector(store=>store);
+
   return (
     <div className="flex flex-wrap justify-between py-5 lg:px-20 border-b">
-      {[1,1,1,1,1,1,1].map((item) => (
+      {homePage.homePageData?.electricCategories?.map((item) => (
           <ElectronicsCategoryCard item={item}/>
         ))}
     </div>
